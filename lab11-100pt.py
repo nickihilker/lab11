11c11c2b3c159e5cbedc50f0969c0637211cbf28
nickihilker <nickihilker@gmail.com>
2014-11-04 13:15:39 -0600

    Final Commit

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -16,20 +16,22 @@ drawpad.grid(row=0, column=0)
 
 # Create the oval
 circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
-direction = 1
+direction = 5
 # Create our animation function
 def animate():
     global direction
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
     if x2 > drawpad.winfo_width(): 
-        direction = - 1
+        #change direction to -801 to be one more than -800
+        direction = - 801
     elif x1 < 0:
-        direction = 1
+        direction = 5
+    drawpad.move(circle,0,0)
     #Move our oval object by the value of direction
     drawpad.move(circle,direction,0)
     # Wait for 1 millisecond, then recursively call our animate function
-    drawpad.after(1, animate)
+    drawpad.after(5, animate)
 
 # Kick off our animation
 animate()
--- lab11-70pt.py
+++ lab11-70pt.py
@@ -12,23 +12,24 @@ root = Tk()
 # Create the canvas widget
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=0)
-
+## CHANGE ALL THE 1'S TO 5'S
 # Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
-direction = 1
+#double each coordinate
+circle = drawpad.create_oval(20, 20, 100, 100, fill='green')
+direction = 5
 # Create our animation function
 def animate():
     global direction
     # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
     if x2 > drawpad.winfo_width(): 
-        direction = - 1
+        direction = - 5
     elif x1 < 0:
-        direction = 1
+        direction = 5
     #Move our oval object by the value of direction
     drawpad.move(circle,direction,0)
     # Wait for 1 millisecond, then recursively call our animate function
-    drawpad.after(1, animate)
+    drawpad.after(5, animate)
 
 # Kick off our animation
 animate()
